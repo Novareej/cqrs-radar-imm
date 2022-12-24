1d71a68b78ce36f6cf3e9a32b171a6147471c8bc
Arij Bouhlala <Bouhlalaarij@gmail.com>
2022-12-24 20:47:43 +0100

    upd radar

--- radar-service/src/main/java/ma/enset/RadarServiceApplication.java
+++ radar-service/src/main/java/ma/enset/RadarServiceApplication.java
@@ -1,7 +1,14 @@
 package ma.enset;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RadarServiceApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(RadarServiceApplication.class, args);
     }
+
 }
