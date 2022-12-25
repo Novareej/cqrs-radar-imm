f03bdef00e474611e6bdf0fa335148e036148754
Arij Bouhlala <Bouhlalaarij@gmail.com>
2022-12-25 15:10:59 +0100

    Immatriculation-service upd

---
+++ common-api/src/main/java/ma/enset/ImmatriculationServiceApplication.java
@@ -0,0 +1,10 @@
+package ma.enset;
+
+import org.springframework.boot.SpringApplication;
+
+public class ImmatriculationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImmatriculationServiceApplication.class, args);
+    }
+
+}
